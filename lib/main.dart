33c6e6e7a9eaf8417c1cf998754b05771162f1d0
achyu <achyut1234duggal@gmail.com>
2023-08-22 12:10:10 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/user_services.dart';
+import 'services/route_service.dart';
+
+import 'menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snake Game',
+      debugShowCheckedModeBanner: false,
+      home: MultiProvider(providers: [
+        ChangeNotifierProvider<UserService>(
+          create: (_) => UserService(),
+        ),
+        ChangeNotifierProvider<RouteService>(
+          create: (_) => RouteService(),
+        ),
+      ], child: Menu()),
+    );
+  }
+}
+
